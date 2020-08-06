9614ec74ea16592069cb52f89f08d81dc38a7c8c
Aigerim <aigerimkadyrbaeva92@gmail.com>
2020-08-05 20:02:46 -0500

    add Aigerim.java

---
+++ src/utility/Aigerim.java
@@ -0,0 +1,13 @@
+package utility;
+
+public class Aigerim {
+    public static void main(String[] args) {
+
+        System.out.println("It was my pleasure to be with you......");
+
+
+
+
+
+    }
+}
